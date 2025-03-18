9851a2b5c6dc331912311ef60229975c30bb4fed
fasare <fasare@student.42wolfsburg.de>
2025-03-18 19:29:04 +0100

    informatics exercise

---
+++ sphere.py
@@ -0,0 +1,15 @@
+#Surface Area of Sphere = 4πr²
+
+def surface_area_sphere():
+	pi = 3.14
+	r = 2 #the unit is metre
+	surface_area = 4 * pi * r**2
+	return (surface_area)
+print("The Surface Area of a Sphere is {:.2f} m²".format(surface_area_sphere()))
+#print("\n")
+
+from math import pi
+
+r = 2
+surface_area = 4 * pi * r * r
+print(f"The Surface Area of a Sphere is {surface_area:.2f} m²")
