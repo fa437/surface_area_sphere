da6bcf0b90b1dae27a11f9aff974195c610e6e53
fasare <fasare@student.42wolfsburg.de>
2025-03-19 12:10:51 +0100

    made the parameter r instead of x

--- sphere.py
+++ sphere.py
@@ -16,9 +16,9 @@ surface_area = 4 * pi * r**2 #Return the calculated area
 print(f"The Surface Area of a Sphere is {surface_area:.2f} m²")
 
 #Approach 3
-def surface_area_sphere(x):
+def surface_area_sphere(r):
     pi = 3.14
-    surface_area = 4 * pi * x**2
+    surface_area = 4 * pi * r**2
     return (surface_area)  #Return the calculated area
 
 print("The surface area of a sphere is", surface_area_sphere(2), "m²") # Calling the function with a radius
